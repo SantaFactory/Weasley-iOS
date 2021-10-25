2072136a82f055f5d0fd4c3b0ae9a59f3355e155
iDoyoung <ido.young@icloud.com>
2021-10-25 22:09:25 +0900

    Set Initialize

---
+++ Weasley/Weasley/MainViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Weasley
+//
+//  Created by Doyoung on 2021/10/25.
+//
+
+import UIKit
+import SnapKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .systemBackground
+    }
+
+
+}
+
