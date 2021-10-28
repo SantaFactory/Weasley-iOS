37a3cfb41cacc2297320390d938a180a58f923cc
iDoyoung <ido.young@icloud.com>
2021-10-28 23:02:42 +0900

    Add Google Log in Button

--- Weasley/Weasley/AppDelegate.swift
+++ Weasley/Weasley/AppDelegate.swift
@@ -6,6 +6,8 @@
 //
 
 import UIKit
+import Firebase
+import GoogleSignIn
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -13,12 +15,27 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        FirebaseApp.configure()
+        
+        GIDSignIn.sharedInstance.restorePreviousSignIn { user, error in
+            if error != nil || user == nil {
+                print("Signed-Out State")
+                // Show the app's signed-out state.
+            } else {
+                print("Signed-In State")
+                // Show the app's signed-in state.
+            }
+        }
         return true
     }
-
-    // MARK: UISceneSession Lifecycle
-
+    
+    func application(_ application: UIApplication, open url: URL,
+                     options: [UIApplication.OpenURLOptionsKey: Any])
+    -> Bool {
+        print(GIDSignIn.sharedInstance.handle(url))
+        return GIDSignIn.sharedInstance.handle(url)
+    }
+    
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
         // Called when a new scene session is being created.
         // Use this method to select a configuration to create the new scene with.
---
+++ Weasley/Weasley/LoginViewController.swift
@@ -0,0 +1,62 @@
+//
+//  SiginViewController.swift
+//  Weasley
+//
+//  Created by Doyoung on 2021/10/26.
+//
+
+import UIKit
+import SnapKit
+import Firebase
+import GoogleSignIn
+
+class LoginViewController: UIViewController {
+
+    private lazy var googleLoginButton: UIButton = {
+        let button = UIButton()
+        button.backgroundColor = #colorLiteral(red: 0.2745098039, green: 0.5333333333, blue: 0.9333333333, alpha: 1)
+        button.layer.cornerRadius = 25
+        button.setTitle("Continue with Google", for: .normal)
+        button.tintColor = .white
+        button.addTarget(self, action: #selector(googleLogin), for: .touchUpInside)
+        return button
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .systemBackground
+        self.view.addSubview(googleLoginButton)
+        googleLoginButton.snp.makeConstraints { make in
+            make.height.equalTo(50)
+            make.bottom.equalToSuperview().offset(-40)
+            make.leading.equalToSuperview().offset(20)
+            make.trailing.equalToSuperview().offset(-20)
+        }
+    }
+
+}
+
+extension LoginViewController {
+    
+    @objc private func googleLogin() {
+        guard let clientID = FirebaseApp.app()?.options.clientID else { return }
+        let config = GIDConfiguration(clientID: clientID)
+        GIDSignIn.sharedInstance.signIn(with: config, presenting: self) { user, error in
+            guard error == nil else {
+                print("Error\(error?.localizedDescription ?? "Can't find error")")
+                return
+            }
+            guard let user = user else { return }
+            //guard let accessToken = user.authentication.idToken else { return }
+            
+            user.authentication.do { authentication, error in
+                guard error == nil else { return }
+                guard let authentication = authentication else { return }
+
+                let idToken = authentication.idToken
+
+            }
+        }
+    }
+    
+}
