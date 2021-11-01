f26834b6aa47a13505b73a25d19b52b1a013d4a0
iDoyoung <ido.young@icloud.com>
2021-11-01 14:26:36 +0900

    Add Sign out Button in Main

--- Weasley/Weasley/MainViewController.swift
+++ Weasley/Weasley/MainViewController.swift
@@ -6,14 +6,35 @@
 //
 
 import UIKit
+import GoogleSignIn
 import SnapKit
 
 class MainViewController: UIViewController {
 
+    private lazy var signOutButton: UIButton = {
+        let button = UIButton()
+        button.setImage(UIImage(systemName: "escape"), for: .normal)
+        button.tintColor = .systemYellow
+        return button
+    }()
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = .systemBackground
+        self.view.addSubview(signOutButton)
+        signOutButton.snp.makeConstraints { make in
+            make.height.equalTo(50)
+            make.width.equalTo(50)
+            make.trailing.equalToSuperview().offset(-20)
+            make.top.equalTo(self.view.safeArea.top).offset(20)
+        }
     }
 
 }
 
+extension MainViewController {
+    
+    @objc func signOut() {
+        print("Sign Out")
+        GIDSignIn.sharedInstance.signOut()
+    }
+}
