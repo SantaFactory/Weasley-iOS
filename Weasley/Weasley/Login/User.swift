d2753e5e0cc4e57321a9583e3ceaaa68e24c1a46
iDoyoung <ido.young@icloud.com>
2021-11-09 13:29:08 +0900

    Update User

--- Weasley/Weasley/Login/User.swift
+++ Weasley/Weasley/Login/User.swift
@@ -8,20 +8,8 @@
 import Foundation
 
 struct User: Codable {
-    let iss: String
-    let azp: String
     let sub: String
     let email: String
-    let email_verified: Bool
-    let at_hash: String
-    let nonce: String
-    let name: String
-    let picture: String
-    let given_name: String
-    let family_name: String
-    let locale: String
-    let iat: String
-    let exp: String
 }
 
 struct Token: Codable {
