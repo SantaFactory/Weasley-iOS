496ffd5e88704e7ccf0964c06a4e254d996a8db7
iDoyoung <ido.young@icloud.com>
2021-12-15 12:35:16 +0900

    Initial Set Home View

---
+++ Weasley/Weasley/InitialView/SetHomeViewController.swift
@@ -0,0 +1,19 @@
+//
+//  SetHomeViewController.swift
+//  Weasley
+//
+//  Created by Doyoung on 2021/12/15.
+//
+
+import UIKit
+import MapKit
+import SnapKit
+
+class SetHomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+}
