d2c7ef749c9a63c63b7fe999eb2af3168be1916b
iDoyoung <ido.young@icloud.com>
2021-12-15 13:59:15 +0900

    Initial Set School View

---
+++ Weasley/Weasley/InitialView/SetSchoolViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SetSchoolViewController.swift
+//  Weasley
+//
+//  Created by Doyoung on 2021/12/15.
+//
+
+import UIKit
+
+class SetSchoolViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+}
