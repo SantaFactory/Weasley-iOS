6ccd120cad8dc49aa5b1adba28d6f946c32794c4
iDoyoung <ido.young@icloud.com>
2021-12-17 12:35:37 +0900

    Add ProfileVC

---
+++ Weasley/Weasley/SettingView/ProfileViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ProfileViewController.swift
+//  Weasley
+//
+//  Created by Doyoung on 2021/12/17.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.title = "Profile"
+    }
+    
+}
