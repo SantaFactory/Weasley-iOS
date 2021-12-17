3eff3a11d052b6770fa59eb84d80436f7554c381
iDoyoung <ido.young@icloud.com>
2021-12-17 15:22:52 +0900

    Add delegate for mapkit and coreloaction

--- Weasley/Weasley/SettingView/EditPlaceViewController.swift
+++ Weasley/Weasley/SettingView/EditPlaceViewController.swift
@@ -7,12 +7,17 @@
 
 import UIKit
 import MapKit
+import CoreLocation
 import SnapKit
 
 class EditPlaceViewController: UIViewController {
 
     var place: String?
+    let locationManager = CLLocationManager()
     var placeLocation = "Latitude: lat \nLongitude: long"
+    
+    var lat: String?
+    var long: String?
     
     override func loadView() {
         super.loadView()
@@ -62,6 +67,13 @@ class EditPlaceViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         self.title = "Edit Place"
+        mapView.delegate = self
+        locationManager.delegate = self
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        markLocation()
     }
     
     private lazy var titleLabel: UILabel = {
@@ -91,6 +103,7 @@ class EditPlaceViewController: UIViewController {
     private lazy var requestLocationButton: UIButton = {
         let button = UIButton(type: .system)
         button.setTitle("Request current location", for: .normal)
+        button.addTarget(self, action: #selector(requestCurrentLocation), for: .touchUpInside)
         return button
     }()
     
@@ -100,4 +113,55 @@ class EditPlaceViewController: UIViewController {
         return button
     }()
     
+    func markLocation() {
+        mapView.removeOverlays(mapView.overlays)
+        guard let latitude = lat, let longitude = long else {
+            return
+        }
+        let lat = Double(latitude)!
+        let long = Double(longitude)!
+        let loc = CLLocationCoordinate2D(latitude: lat, longitude: long)
+        let circle = MKCircle(center: loc, radius: 200)
+        let span = MKCoordinateSpan(latitudeDelta: 0.005, longitudeDelta: 0.005)
+        mapView.setRegion(MKCoordinateRegion(center: loc, span: span), animated: true)
+        mapView.addOverlay(circle)
+    }
+}
+
+extension EditPlaceViewController {
+    @objc func requestCurrentLocation() {
+        locationManager.requestLocation()
+    }
+}
+
+//MARK: CORE LOCATION MANGER DELEGATE
+extension EditPlaceViewController: CLLocationManagerDelegate {
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let location = locations.last {
+            locationManager.stopUpdatingLocation()
+            let digit: Double = pow(10, 3)
+            self.lat = String(round(location.coordinate.latitude * digit) / digit)
+            self.long = String(round(location.coordinate.longitude * digit) / digit)
+            markLocation()
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print("Cancel Location")
+        print("Error: \(error.localizedDescription)")
+    }
+    
+}
+
+extension EditPlaceViewController: MKMapViewDelegate {
+    func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
+        guard let circleOverlay = overlay as? MKCircle else {
+            return MKOverlayRenderer()
+        }
+        let circleRenderer = MKCircleRenderer(overlay: circleOverlay)
+        circleRenderer.fillColor = .systemBlue
+        circleRenderer.alpha = 0.7
+        return circleRenderer
+    }
 }
