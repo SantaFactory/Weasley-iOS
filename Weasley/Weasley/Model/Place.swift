44b5b8fc7ba317a6a04670f8f4404d01f5afc4ab
iDoyoung <ido.young@icloud.com>
2021-12-21 17:25:34 +0900

    Add Model

---
+++ Weasley/Weasley/Model/Place.swift
@@ -0,0 +1,14 @@
+//
+//  Place.swift
+//  Weasley
+//
+//  Created by Doyoung on 2021/12/21.
+//
+
+import Foundation
+
+struct Place {
+    var place: String?
+    var latitude: String?
+    var longitude: String?
+}
