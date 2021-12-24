8e11b8c88bb32a784e5703370580bd1eb696caf6
iDoyoung <ido.young@icloud.com>
2021-12-24 15:42:22 +0900

    Initial View model

---
+++ Weasley/Weasley/ViewModel/CurrentGroup.swift
@@ -0,0 +1,19 @@
+//
+//  CurrentGroup.swift
+//  Weasley
+//
+//  Created by Doyoung on 2021/12/24.
+//
+
+import Foundation
+
+class CurrentGroup {
+    
+    var group: SampleGroup!
+    
+    init(id: String, completion: @escaping () -> Void) {
+        //TODO: API service
+        
+    }
+    
+}
