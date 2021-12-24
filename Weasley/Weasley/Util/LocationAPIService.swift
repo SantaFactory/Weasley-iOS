4f3b16850ad06b432913766d135e8efde7b16bda
iDoyoung <ido.young@icloud.com>
2021-12-24 16:59:08 +0900

    Initial API service

---
+++ Weasley/Weasley/Util/LocationAPIService.swift
@@ -0,0 +1,15 @@
+//
+//  LocationAPIService.swift
+//  Weasley
+//
+//  Created by Doyoung on 2021/12/24.
+//
+
+import Foundation
+
+class LocationAPIService {
+    
+    func performPostLocation(user: String) {
+       //TODO: Add post location API Service
+    }
+}
