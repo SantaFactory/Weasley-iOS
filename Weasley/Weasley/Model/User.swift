152ac8ade834d6e08c9d45654be61aa4342a34e7
iDoyoung <ido.young@icloud.com>
2021-12-25 15:43:15 +0900

    Add Result Token model

--- Weasley/Weasley/Model/User.swift
+++ Weasley/Weasley/Model/User.swift
@@ -12,6 +12,20 @@ struct Token: Codable {
     let id_token: String
 }
 
+struct ResultToken: Codable {
+    let loginData: LoginData
+    
+    enum CodingKeys: String, CodingKey {
+        case loginData = "data"
+    }
+}
+
+struct LoginData: Codable {
+    let email: String
+    let sub: String
+    let token: String
+}
+
 struct User: Codable {
     let sub: String
     let email: String
