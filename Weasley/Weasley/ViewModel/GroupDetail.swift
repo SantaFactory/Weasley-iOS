0aad94ad59c4659720d90a072272c0e9e39ea00e
iDoyoung <ido.young@icloud.com>
2022-01-11 20:14:15 +0900

    Add Detail View Model

---
+++ Weasley/Weasley/ViewModel/GroupDetail.swift
@@ -0,0 +1,15 @@
+//
+//  GroupDetail.swift
+//  Weasley
+//
+//  Created by Doyoung on 2022/01/11.
+//
+
+import Foundation
+
+struct Detail {
+    
+    //TODO: Member List
+    //TODO: Member Place
+    //TODO: Invite Member
+}
