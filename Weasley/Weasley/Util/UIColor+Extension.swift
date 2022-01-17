0f0b943294217a84e20c5972ce2b7590f530c727
iDoyoung <ido.young@icloud.com>
2022-01-17 12:34:54 +0900

    Update UIColor extension

--- Weasley/Weasley/Util/UIColor+Extension.swift
+++ Weasley/Weasley/Util/UIColor+Extension.swift
@@ -2,35 +2,14 @@
 //  UIColor+Extension.swift
 //  Weasley
 //
-//  Created by Doyoung on 2021/12/18.
+//  Created by Doyoung on 2022/01/17.
 //
 
 import UIKit
 
 extension UIColor {
     
-    var themeColors: [UIColor] {
-        return [
-            UIColor(named: "magicRed")!,
-            UIColor(named: "magicOrange")!,
-            UIColor(named: "magicYellow")!,
-            UIColor(named: "magicLime")!,
-            UIColor(named: "magicGreen")!,
-            UIColor(named: "magicBlue")!,
-            UIColor(named: "magicPurple")!,
-            UIColor(named: "magicPink")!
-        ]
+    class var themeGreen: UIColor {
+        return UIColor(named: "themeGreen")!
     }
-    
-    static func gradientColor(bounds: CGRect, gradientLayer :CAGradientLayer) -> UIColor? {
-        UIGraphicsBeginImageContext(gradientLayer.bounds.size)
-        guard let context = UIGraphicsGetCurrentContext() else {
-            return .systemBackground
-        }
-        gradientLayer.render(in: context)
-        let image = UIGraphicsGetImageFromCurrentImageContext()
-        UIGraphicsEndImageContext()
-        return UIColor(patternImage: image!)
-    }
-    
 }
